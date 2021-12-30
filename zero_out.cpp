87bc175d4bef177bf70405a443ad96d663162f3a
Rangsiman Ketkaew <rangsiman1993@gmail.com>
2021-12-30 16:13:07 +0100

    Create zero_out.cpp

---
+++ zero_out.cpp
@@ -0,0 +1,12 @@
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+
+using namespace tensorflow;
+
+REGISTER_OP("ZeroOut")
+    .Input("to_zero: int32")
+    .Output("zeroed: int32")
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+      c->set_output(0, c->input(0));
+      return Status::OK();
+    });
